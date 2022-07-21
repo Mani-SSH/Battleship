f44315c6fe5796cfc7b6aa3f20196dd7e9dca48b
Re-Dei <hridaya.pradhan123@gmail.com>
2022-07-21 15:30:34 +0545

    made folder for components

--- battleship/src/index.js
+++ battleship/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './Home';
+import Home from './components/Home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
