304ad7e76e1a114ccef5a8ea56d654eae91fc6a9
Re-Dei <hridaya.pradhan123@gmail.com>
2022-08-05 20:11:38 +0545

    made ui for create room

--- battleship/front-end/src/pages/Home/components/CreateRoom.jsx
+++ battleship/front-end/src/pages/Home/components/CreateRoom.jsx
@@ -2,7 +2,37 @@ import React, { useState } from 'react';
 import Modal from 'react-modal';
 import '../../../assets/css/Home.css';
 
-/**
+var handleGenerateRoom = () => {
+    
+}
+
+export default function CreateRoom() {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    function toggleModal() {
+      setIsOpen(!isOpen);
+    }
+  
+    return (
+        <div id='call1' className='Homie'>
+            <button className='button-basic1'onClick={toggleModal}>Create room</button>
+
+            <Modal
+                isOpen={isOpen}
+                onRequestClose={toggleModal}
+                contentLabel='Create room'
+            >
+                <div>Create Room</div>
+                <input placeholder='Enter room no.' maxlength="6"></input>
+                <button>Join</button>
+                <button onClick={handleGenerateRoom}>Generate Room</button>
+                <button onClick={toggleModal}>Close</button>
+            </Modal>
+        </div>
+    );
+  }
+
+  /**
 class CreateRoom extends Component{
     state = {
         isOpen : false,
@@ -36,27 +66,3 @@ class CreateRoom extends Component{
     }
 }
 */
-
-export default function CreateRoom() {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    function toggleModal() {
-      setIsOpen(!isOpen);
-    }
-  
-    return (
-        <div id='call1' className='Homie'>
-            <button className='button-basic1'onClick={toggleModal}>Create room</button>
-
-            <Modal
-                isOpen={isOpen}
-                onRequestClose={toggleModal}
-                contentLabel='My dialog'
-            >
-                <div>Create Room</div>
-                <button>Generate Room</button>
-                <button onClick={toggleModal}>Close</button>
-            </Modal>
-        </div>
-    );
-  }
