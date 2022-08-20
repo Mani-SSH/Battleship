419a70b6220a843ecc5397df19a6a082cbe56ccb
Re-Dei <hridaya.pradhan123@gmail.com>
2022-08-20 18:41:19 +0545

    made linked list to store rooms

--- battleship/back-end/server.js
+++ battleship/back-end/server.js
@@ -1,10 +1,11 @@
-/* length of roomID */
-const LENGTH_ROOMID = 6;
-
 /* connect socket io with express */
 const express = require('express');
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+
+const room = require("./room");
+
+let rooms = new room.LinkedList;
 
 /* port  */
 let PORT = process.env.PORT||5000;
@@ -26,9 +27,11 @@ io.on('connection', (socket) => {
 
     /* listen to event on a socket to generate roomID */
     socket.on('generate-roomID', () => {
-        let roomID = makeRoomID();
-        console.log(`new room: ${roomID} generated`);
-        socket.emit('send-roomID', roomID);
+        var thisRoom = new room.Room;
+        console.log(`new room: ${thisRoom.roomID} generated`);
+        rooms.add(thisRoom);
+        socket.emit('send-roomID', thisRoom.roomID);
+        rooms.display();
     })
 })
 
@@ -39,18 +42,3 @@ const mongoose = require('mongoose');
 mongoose.connect(dbUrl, (err) => {
     console.log('mongodb connected', err);
 })
-
-/**
- * 
- * @returns 6 charactered roomID
- */
-function makeRoomID() {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < LENGTH_ROOMID; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * 
-        charactersLength));
-   }
-   return result;
-}
