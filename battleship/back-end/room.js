14feabafa195a9cfaa9277e01f49e143c74f26d7
Re-Dei <hridaya.pradhan123@gmail.com>
2022-08-25 15:29:46 +0545

    made the background music looping

--- battleship/back-end/room.js
+++ battleship/back-end/room.js
@@ -112,6 +112,5 @@ class RoomList{
    }
 }
 
-
 exports.RoomList = RoomList;
 exports.Room = Room;
--- battleship/front-end/src/pages/Home/components/sound.jsx
+++ battleship/front-end/src/pages/Home/components/sound.jsx
@@ -4,27 +4,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../../assets/css/audio.sass';
 import background from "../../../assets/bgm/Home/background.mp3";
 
+/**
+ * @returns background music with button to toggle it on and off for home page
+ */
 export default function Music()
 {
-    const [audio] = useState(new Audio(background));
-    const [playing, setPlaying] = useState(false);
+    const [audio] = useState(new Audio(background));    //background music
+    const [playing, setPlaying] = useState(false);      //status of music
+    const toggle = () => setPlaying(!playing);          //handler for toggling music on and off
 
-    const toggle = () => setPlaying(!playing);
+    /* set audio to loop */
+    audio.loop = true;
 
+    /* side effect of 'playing' state being changed */
     useEffect(() => {
+        /* change audion to play and pause when 'playing' changes */
         playing ? audio.play() : audio.pause();
     },[playing, audio]);
 
-    useEffect(() => {
-    audio.addEventListener('ended', () => setPlaying(false));
-    return () => {
-      audio.removeEventListener('ended', () => setPlaying(false));
-    }
-    },[audio]);
-
         return(
             <div>
-                <Button className="audi" size='md' bsPrefix="Home" onClick={toggle}>{playing?'Pause':'Play'}</Button>
+                <Button className="audi" size='md' bsPrefix="Home" onClick={ toggle }>{ playing? 'Pause': 'Play' }</Button>
             </div>
         );
 
