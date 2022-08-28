2de5d4f866e4a6b3c2f3233a4e23dcd9973bdf24
BigyanByanju <byanjubigyan9841@gmail.com>
2022-08-28 21:48:03 +0545

    Auto play fixed

--- battleship/front-end/src/pages/Home/components/sound.jsx
+++ battleship/front-end/src/pages/Home/components/sound.jsx
@@ -10,15 +10,16 @@ import background from "../../../assets/bgm/Home/background.mp3";
 export default function Music()
 {
     const [audio] = useState(new Audio(background));    //background music
-    const [playing, setPlaying] = useState(false);      //status of music
+    const [playing, setPlaying] = useState(true);      //status of music
     const toggle = () => setPlaying(!playing);          //handler for toggling music on and off
 
     /* set audio to loop */
     audio.loop = true;
-
+    
     /* side effect of 'playing' state being changed */
     useEffect(() => {
-        /* change audion to play and pause when 'playing' changes */
+        /* change audio to play and pause when 'playing' changes */
+        
         playing ? audio.play() : audio.pause();
     },[playing, audio]);
 
