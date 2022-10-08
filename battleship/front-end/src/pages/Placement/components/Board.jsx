690d9d6a9701b620bce35cc5f77aaa7a20dd7e56
Re-Dei <hridaya.pradhan123@gmail.com>
2022-10-08 19:55:42 +0545

    made highlight of invalid condition

--- battleship/front-end/src/pages/Placement/components/Board.jsx
+++ battleship/front-end/src/pages/Placement/components/Board.jsx
@@ -8,24 +8,64 @@ import { Ships, ShipPreview} from "./Ships";
 
 //const xyIntoPosition = (x, y) => ((x - 1) * 10 + (y - 1 * x))
 
+/**
+ * 
+ * @param {array} coordinates 
+ * @returns true if all coordinates are valid, else false
+ */
+const checkValid = (coordinates) => {
+    /* if any coordinate is out of board, return false */
+    for(let i = 0; i < coordinates.length; i++){
+        if(coordinates[i][0] < 1 || coordinates[i][1] < 1){
+            return false
+        }
+    }
+
+    /* if any coordinate is coincides with coordinates of other ship, return false */
+
+
+    return true
+}
+
+/**
+ * @param {number} x 
+ * @param {number} y 
+ * @param {number} length 
+ * @returns array of XYs adjacent to square in (x, y)
+ */
+const getAdjacentXYs = (x, y, length) => {
+    const adjacentXYs = []
+
+    /* for horizontal ship */
+    for(let i = 0; i < length; i++){
+        adjacentXYs.push([x, y - i])
+    }
+
+    // console.log(adjacentXYs)
+    return adjacentXYs
+}
+
 export default function Board()
 {
     let board =[];
     
-    const [ship, setShip] = useState()
+    const [ship, setShip] = useState() // ship clicked
 
-    const [currentXY, setCurrentXY] = useState({x:0, y: 0})
-    const [hoverXYs, setHoverXYs] = useState([])
-    const [clickedXY, setClickedXY] = useState({x: 0, y: 0})
+    const [currentXY, setCurrentXY] = useState({x:0, y: 0}) // coordinates of current cell pointed on board
+    const [hoverXYs, setHoverXYs] = useState([]) // coordinates of adjacent cells where ship is placed
+    const [clickedXY, setClickedXY] = useState({x: 0, y: 0}) // coordinates of clicked cell
     const [shipCordinates, setShipCordinates] = useState([])
 
     const [rotateShip,setRotation] = useState(0)
 
-    const [resetHighlight, setResetHighlight] = useState(false)
+    const [valid, setValid] = useState(true) // if the adjacent cells are valid
+
+    const [resetHighlight, setResetHighlight] = useState(false) // toggles when mouse in on and off the board, turns off highlight
 
     const coordinates = useContext(CoordinatesContext)
     const setCoordinates = useContext(CoordinatesUpdateContext)
 
+    /* setting board */
     for(let j=1; j <= 9; j++)
     {
         for(let i = 1; i <= 9; i++)
@@ -41,39 +81,22 @@ export default function Board()
                 ship={ ship }
                 hoverXYs={ hoverXYs }
                 resetHighlight={ resetHighlight }
+                valid={ valid }
                 />
             );
         }
     }
 
+    /**
+     * when mouse is off the board, resets highlight, current coordinates and adjacent coordinates
+     */
     const handleMouseLeaveBoard = () => {
         setResetHighlight(true)
         setCurrentXY({x:0, y:0})
         setHoverXYs([])
     }
 
-    /**
-     * @param {number} x 
-     * @param {number} y 
-     * @param {number} length 
-     * @returns array of XYs adjacent to square in (x, y)
-     */
-    const getAdjacentXYs = (x, y, length) => {
-        const adjacentXYs = []
-
-        /* for horizontal ship */
-        for(let i = 0; i < length; i++){
-            if((y-i)<=0)
-            {
-                console.log("Out of scope")
-                break;
-            }
-                adjacentXYs.push([x, y - i])
-        }
-
-        // console.log(adjacentXYs)
-        return adjacentXYs
-    }
+    
 
 
     /* REMOVE LATER */
@@ -84,17 +107,25 @@ export default function Board()
 
     /* on hover while ship is clicked */
     useEffect(() => {
+        /* check if ship is clicked */
         if(ship){
+            /* check if currentXY is pointing in board */
             if(!(currentXY.x === 0 && currentXY.y === 0)){
+                /* get length of ship and current x and y */
                 const { length } = ship
                 const { x, y } = currentXY
+
+                /* get array of adjecent cells */
                 let adjacentXYs = getAdjacentXYs(x, y, length)
-                // console.log(adjacentXYs)
+
+                /* check if they are valid and set value of valid to the value returned */
+                setValid(checkValid(adjacentXYs))
+
+                /* set value of adjacent coordinates on hover */
                 setHoverXYs(adjacentXYs)
-                console.log(hoverXYs)
             }
         }
-    }, [ship, currentXY])
+    }, [ship, currentXY]) // eslint-disable-line react-hooks/exhaustive-deps
 
     useEffect(() => {
         if(ship){
@@ -110,7 +141,7 @@ export default function Board()
                 console.log(shipCordinates)
             }
         }
-    }, [ship, clickedXY])
+    }, [ship, clickedXY]) 
 
     
 
@@ -139,11 +170,14 @@ export default function Board()
 }
 
 
-function Square({ x, y, setXY, setClicked, ship, hoverXYs, resetHighlight }){
-    const [colour, setColour] = useState("white")
+function Square({ x, y, setXY, setClicked, ship, hoverXYs, resetHighlight, valid }){
+    const [colour, setColour] = useState("white") // color of the cell
 
+    /* when mouse hovers on the cell */
     const handleHover = () => {
+        /* if ship is clicked */
         if(ship){
+            /* sets value of current xy */
            setXY({x, y})
         }
     }
@@ -154,17 +188,30 @@ function Square({ x, y, setXY, setClicked, ship, hoverXYs, resetHighlight }){
         }
     }
 
+    /* used for highlight */
     useEffect(() => {
+        /* for every adjacent coordintes */
         for(let i = 0; i < hoverXYs.length; i++){
+            /* if coordinates match */
             if(hoverXYs[i][0] === x && hoverXYs[i][1] === y){
-                setColour("green")
-                break
+                /* check validity */
+                if(valid){
+                    /* highlight green */
+                    setColour("green")
+                    break
+                }else{
+                    /* highlight red */
+                    setColour("red")
+                    break
+                }
             }else{
+                /* default color */
                 setColour("white")
             }
         }
-    }, [hoverXYs])
+    }, [hoverXYs]) // eslint-disable-line react-hooks/exhaustive-deps
 
+    /* if mouse is off the board, set to default color */
     useEffect(() => {
         if(resetHighlight){
             setColour("white")
@@ -172,6 +219,12 @@ function Square({ x, y, setXY, setClicked, ship, hoverXYs, resetHighlight }){
     }, [resetHighlight])
 
     return(
-        <div className="tiles" onMouseOver={ handleHover } onMouseDown = { handleClick }><h6>{colour}</h6></div>
+        <div
+        className="tiles"
+        onMouseOver={ handleHover }
+        onMouseDown = { handleClick }
+        >
+            <h6>{colour}</h6>
+        </div>
     )
 }
