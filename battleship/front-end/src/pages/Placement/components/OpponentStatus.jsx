a8f9229be5ebb4fcb0cfb4c478b70a0c108217b5
Re-Dei <hridaya.pradhan123@gmail.com>
2022-10-19 20:55:14 +0545

    Update OpponentStatus.jsx

--- battleship/front-end/src/pages/Placement/components/OpponentStatus.jsx
+++ battleship/front-end/src/pages/Placement/components/OpponentStatus.jsx
@@ -1,8 +1,10 @@
+import {AiFillCheckCircle} from "react-icons/ai";
+
 export default function OpponentStatus({ opponentID, ready }){
     return(
         <div>
+            <h3>{ (ready)? <><AiFillCheckCircle className='check' /></> : <div className='spinner-border text-primary m-0.2' role="status"></div> }</h3>
             <h2>{ opponentID }</h2>
-            <h3>{ (ready)? "Ready" : <div className='spinner-border text-primary m-0.2' role="status"></div> }</h3>
         </div>
     )
 }
