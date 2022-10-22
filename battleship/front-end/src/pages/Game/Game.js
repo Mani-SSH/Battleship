b28e43176baf24b341c001cde69e90a5a663de0e
Re-Dei <hridaya.pradhan123@gmail.com>
2022-10-22 15:35:32 +0545

    made player forfeit game

--- battleship/front-end/src/pages/Game/Game.js
+++ battleship/front-end/src/pages/Game/Game.js
@@ -2,6 +2,7 @@ import "../../../src/assets/css/Game.sass";
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Boards from "./components/Boards";
+import * as io from "../../io-client-handler"
 
 
 export default function Game()
@@ -9,6 +10,10 @@ export default function Game()
     const location = useLocation()
     const navigate = useNavigate()
 
+    io.socket.off("player-forfeit").on("player-forfeit", () => {
+        alert("Opponent left the game.")
+        navigate("/")
+    })
     
     return(
         <div className="Gamee">
