2fdabe726c123a5325d3162913d9ec5fb4cf1a54
Re-Dei <hridaya.pradhan123@gmail.com>
2022-11-07 20:00:41 +0545

    Update io-client-handler.js

--- battleship/front-end/src/io-client-handler.js
+++ battleship/front-end/src/io-client-handler.js
@@ -1,2 +1,2 @@
 import { io } from "socket.io-client";
-export const socket = io("https://battleship-isg1.onrender.com", { transports: ['websocket'], upgrade: false });
+export const socket = io("https://battleship-isg1.onrender.com");
